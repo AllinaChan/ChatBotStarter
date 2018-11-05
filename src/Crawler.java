50902655cfc992c7e75d2a555c0ae0703b9fac7c
BT_1N3_31 <zchen5143@bths.edu>
2018-11-05 14:55:54 -0500

    Ayy

--- src/Crawler.java
+++ src/Crawler.java
@@ -16,6 +16,8 @@ public class Crawler {
     private Matcher matcher;
     private static final String DOMAIN_NAME_PATTERN
             = "([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}";
+
     static {
         patternDomainName = Pattern.compile(DOMAIN_NAME_PATTERN);
+    }
 }
