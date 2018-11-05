d9ef1ec8a756bd7015383fc61b05721ef67456af
BT_1N3_31 <zchen5143@bths.edu>
2018-11-05 15:10:21 -0500

    Ayykik

--- src/Crawler.java
+++ src/Crawler.java
@@ -10,6 +10,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+//lol
 public class Crawler {
 
     private static Pattern patternDomainName;
