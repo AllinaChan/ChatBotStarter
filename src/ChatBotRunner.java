938d26bf7427d77560aec4a6bd5b532712f3689b
BT_1N3_31 <bchen9694@bths.edu>
2018-11-08 14:53:16 -0500

    fine

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -34,7 +34,7 @@ public class ChatBotRunner
 		if (useurimmagination.equals("LevinBot"))
 		{
 			String statement = "";
-			while (!statement.equals("Bye"))
+			while (!statement.equals("Bye"))a
 			{
 				//Use Logic to control which chatbot is handling the conversation\
 				//This example has only chatbot
